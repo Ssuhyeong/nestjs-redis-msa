9c62d842ffaa4073feb7ed37663dd45875577bc7
Ssuhyeong <tngud124@gmail.com>
2024-08-01 16:08:56 +0900

    🔨feat : magazines api subscribe 코드 작성

---
+++ magazines-service/src/magazines/magazines.controller.ts
@@ -0,0 +1,39 @@
+import { CreateMagazineDTO } from './dto/create-magazine.dto';
+import { Controller } from '@nestjs/common';
+import { MagazinesService } from './magazines.service';
+import { MessagePattern } from '@nestjs/microservices';
+
+@Controller('magazines')
+export class MagazinesController {
+  constructor(private magazinesService: MagazinesService) {}
+
+  // @Get()
+  @MessagePattern({ cmd: 'getMagazines' })
+  async getMagazines() {
+    const magazines = await this.magazinesService.getMagazines();
+    return magazines;
+  }
+
+  // @Get(':magazineID')
+  @MessagePattern({ cmd: 'getMagazineById' })
+  async getBook(magazineID) {
+    const magazine = await this.magazinesService.getMagazine(magazineID);
+    return magazine;
+  }
+
+  // @Post()
+  @MessagePattern({ cmd: 'addMagazine' })
+  async addBook(createBookDTO: CreateMagazineDTO) {
+    const magazine = await this.magazinesService.addMagazine(createBookDTO);
+    return magazine;
+  }
+
+  // @Delete()
+  @MessagePattern({ cmd: 'deleteMagazine' })
+  async deleteBook(query) {
+    const magazines = await this.magazinesService.deleteMagazine(
+      query.magazineID,
+    );
+    return magazines;
+  }
+}
